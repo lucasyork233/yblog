a02a6d1dc5eb23004622b048a7aca717f856f29d
lucas <chatlucasyork@outlook.com>
2026-04-02 23:06:18 +0800

    [Mod] homepage avatar

--- js/router.js
+++ js/router.js
@@ -85,10 +85,11 @@ const Router = {
     else if (hour >= 18) greeting = 'Good Evening';
 
     // <img src="assets/avatar.jpg" alt="Avatar" class="avatar">
+    // <img src="https://user1481.cn.imgto.link/blog_lucky/20260206/head-02.avif" alt="Avatar" class="avatar">
     app.innerHTML = `
       <div class="page home-page">
         <div class="home-card">
-          <img src="https://user1481.cn.imgto.link/blog_lucky/20260206/head-02.avif" alt="Avatar" class="avatar">
+          <img src="assets/avatar.jpg" alt="Avatar" class="avatar">
           <div class="greeting">${greeting}.</div>
           <div class="intro">I'm <a href="#about" class="name">LucasYork</a>.</div>
           <div class="nice-to-meet">Nice to meet you!</div>
@@ -357,11 +358,12 @@ const Router = {
     const app = document.getElementById('app');
 
     // <img src="assets/avatar.jpg" alt="Avatar" class="avatar">
+    // <img src="https://user1481.cn.imgto.link/blog_lucky/20260206/head-02.avif" alt="LucasYork" class="about-avatar">
     app.innerHTML = `
       <div class="container about-page">
         <article class="card">
           <div class="about-header">
-            <img src="https://user1481.cn.imgto.link/blog_lucky/20260206/head-02.avif" alt="LucasYork" class="about-avatar">
+            <img src="assets/avatar.jpg" alt="Avatar" class="avatar">
             <h1 class="about-title">About Me</h1>
           </div>
           <div class="about-content">
